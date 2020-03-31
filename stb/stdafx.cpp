e8292dd23cd6fa571ce5004277549d5be8485897
Johannes Unterguggenberger <johannes.unterguggenberger@gmail.com>
2020-03-31 12:03:30 +0200

    Initial commit with the original stb-files (which do not produce a valid library)

---
+++ stb/stdafx.cpp
@@ -0,0 +1,25 @@
+#include "stdafx.h"
+#include "stb_image.h"
+#include "stb_image_resize.h"
+#include "stb_image_write.h"
+#include "stb_dxt.h"
+#include "stb_perlin.h"
+
+#ifdef _MSC_VER
+#define _CRT_SECURE_NO_WARNINGS
+#endif
+
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+
+#define STB_IMAGE_RESIZE_IMPLEMENTATION
+#include "stb_image_resize.h"
+
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include "stb_image_write.h"
+
+#define STB_DXT_IMPLEMENTATION
+#include "stb_dxt.h"
+
+#define STB_PERLIN_IMPLEMENTATION
+#include "stb_perlin.h"
